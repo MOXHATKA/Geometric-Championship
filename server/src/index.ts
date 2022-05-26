7ad0ab75738d2d7f57c838991192680e2fb60c33
Avirtan <36964697+Avirtan@users.noreply.github.com>
2022-05-26 12:03:28 +0300

    добавлены вебсокеты

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,52 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import socket from "socket.io";
+import http from "http";
+import {
+    ClientToServerEvents,
+    InterServerEvents,
+    ServerToClientEvents,
+    SocketData,
+} from "./socket.interface";
 
+const port = 5000;
 const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
+const server = http.createServer(app);
+const io = new socket.Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+>(server, {
+    cors: {
+      origin: "http://localhost:5000",
+      credentials: true
+    }
+});
+app.use(express.static(__dirname + "/../public"));
+
+io.on("connection", (socket) => {
+    socket.join("room1");
+    socket.emit("noArg");
+    socket.emit("basicEmit", 1, "2", Buffer.from([3]));
+    socket.emit("withAck", "4", (e) => {
+       console.log("withAck");
+    });
+
+    // works when broadcast to all
+    io.emit("noArg");
+
+    // works when broadcasting to a room
+    io.to("room1").emit("basicEmit", 1, "2", Buffer.from([3]));
+});
+io.on("connection", (socket) => {
+    socket.on("hello", () => {
+        // ...
+    });
+});
+
+app.get("/", (req: Request, res: Response) => {
     res.send("The sedulous hyena ate the antelope!");
 });
-app.listen((port: number, err: any) => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
+server.listen(port, () => {
+    console.log("listen port " + port);
 });
--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,28 @@
 import Phaser from 'phaser';
+import { WebSocketPlagin } from './plagins/websocket';
 import Boot from './scenes/Boot';
 import MainGame from './scenes/MainGame';
 import MainMenu from './scenes/MainMenu';
 import Preloader from './scenes/Preloader';
 import UIScene from './scenes/UIScene';
 
-export default {
+var config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: window.innerWidth,
     height: window.innerHeight,
     backgroundColor: '#000000',
     parent: 'phaser-example',
     scene: [Boot, Preloader, MainMenu, MainGame, UIScene],
+    plugins: {
+        global: [
+            { key: 'WebSocketPlagin', plugin: WebSocketPlagin, start:true }
+        ]
+    },
     physics: {
         default: 'arcade',
         arcade: { debug: false },
     },
     pixelArt: true
 }
+
+export default config;
